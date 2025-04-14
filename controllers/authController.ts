9900c9a58c13bd074ff97e875859a37cb5a63ad0
codefromtheslum <kossyuzoigwe@gmail.com>
2025-04-14 11:02:00 +0100

    changes

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,7 +1,7 @@
 import bcryptjs from "bcryptjs";
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
-import { sendVerification } from "../config/emailServices";
+import { sendResetPassword, sendVerification } from "../config/emailServices";
 
 const prisma = new PrismaClient();
 
@@ -67,7 +67,7 @@ export const createPassword: any = async (req: Request, res: Response) => {
       });
     }
 
-    const hashedPassword: any = bcryptjs.hash(password, 10);
+    const hashedPassword = await bcryptjs.hash(password, 10);
 
     const newUser = await prisma.user.update({
       where: { id },
@@ -75,7 +75,6 @@ export const createPassword: any = async (req: Request, res: Response) => {
         firstName,
         lastName,
         password: hashedPassword,
-        verficationCode: null,
       },
     });
 
@@ -93,38 +92,138 @@ export const createPassword: any = async (req: Request, res: Response) => {
   }
 };
 
-export const loginAccount: any = async (req: Request, res: Response) => {
+export const loginAccount = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
   try {
-    const { email, password } = req.body;
+    const { email } = req.body;
 
-    const user = await prisma.user.findUnique({ where: email });
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
 
     if (!user) {
-      return res.status(400).json({
-        message: `The email you've entered does not exist, create an account now`,
+      return res.status(404).json({
+        message: `Account does not exist`,
+      });
+    }
+
+    return res.status(200).json({
+      message: `Almost there...`,
+      data: user,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: `Error occured during login`,
+      data: error?.message,
+    });
+  }
+};
+
+export const checkPassword = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const { email } = req.params;
+
+    const { password } = req.body;
+
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({
+        message: `Account does not exist`,
       });
     }
 
     if (user) {
-      const check = await bcryptjs.compare(password, user.password || "");
+      const check = await bcryptjs.compare(password, user?.password || "");
 
       if (check) {
         return res.status(200).json({
-          message: "Account logged in successfully",
+          message: `Logged in successfully`,
         });
       } else {
         return res.status(400).json({
           message: `Incorrect password`,
         });
       }
-    } else {
-      return res.status(400).json({
-        message: `Check credentials and try again`,
-      });
     }
   } catch (error: any) {
     return res.status(500).json({
-      message: `Error occured during account login`,
+      message: `Error occured validating password`,
+      data: error?.message,
+    });
+  }
+};
+
+export const resetPassword = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const { email } = req.body;
+
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({
+        message: `Account does not exist`,
+      });
+    }
+
+    await sendResetPassword(user);
+
+    return res.status(200).json({
+      message: `Resetting password...`,
+      data: user,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: `Error occured while resetting password`,
+      data: error?.message,
+    });
+  }
+};
+
+export const createNewPassword = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const { id } = req.params;
+    const { password } = req.body;
+
+    const user = await prisma.user.findUnique({ where: { id } });
+
+    if (!user) {
+      return res.status(404).json({
+        message: `Account not found`,
+      });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const newUser = await prisma.user.update({
+      where: { id },
+      data: {
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...hidePassword } = newUser;
+
+    return res.status(200).json({
+      message: `Password updated successfully`,
+      data: hidePassword,
+    });
+
+    
+  } catch (error: any) {
+    return res.status(500).json({
+      message: `Error occured while creating new password`,
       data: error?.message,
     });
   }
--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import {
+  checkPassword,
   createAccount,
+  createNewPassword,
   createPassword,
   loginAccount,
+  resetPassword,
 } from "../controllers/authController";
 
 const router = express.Router();
 router.route("/").post(createAccount);
 router.route("/:id/create-password").patch(createPassword);
-router.route("/login").post(loginAccount);
+router.route("/login").get(loginAccount);
+router.route("/:email/validate-pass").post(checkPassword);
+router.route("/reset-password").post(resetPassword);
+router.route("/:id/complete").patch(createNewPassword);
 
 export default router;
